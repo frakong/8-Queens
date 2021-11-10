ecba5dbc5045e91363fd391c6a664127dac4a3bb
Franklin Kong <frakong@ucdavis.edu>
2021-11-10 13:43:22 -0800

    Added function to check entire board for clashes.

--- Clash_Checker.py
+++ Clash_Checker.py
@@ -25,8 +25,23 @@ def col_clash(positions, col_num):
     return False             # No column clashes
 
 
-print("Beginning column clash tests: ")
-unit_test(not col_clash([6,4,2,0,5], 4))
-unit_test(not col_clash([6,4,2,0,5,7,1,3], 7))
+# print("Beginning column clash tests: ")
+# unit_test(not col_clash([6,4,2,0,5], 4))
+# unit_test(not col_clash([6,4,2,0,5,7,1,3], 7))
 
-unit_test(col_clash([0,1], 1))    
+# unit_test(col_clash([0,1], 1))    
+
+def check_for_clashes(queen_positions):
+    """Determine whether we have any queens clashing on the diagonals of the board with positions of queens represented by queen_positions (index # represents the column, value of each index represents the row).
+    We're assuming here that queen_positions is a permutation of row numbers so we're guaranteed that row and column numbers are unique, only need to check for diagonal clashes."""
+    for column in range(1, len(queen_positions)):
+        if col_clash(queen_positions, column):
+            return True
+    return False
+
+
+print("Beginning check_for_clashes tests: ")
+unit_test(not check_for_clashes([6,4,2,0,5,7,1,3]))
+unit_test(check_for_clashes([4,6,2,0,5,7,1,3]))
+unit_test(check_for_clashes([0,1,2,3]))
+unit_test(not check_for_clashes([2,0,3,1]))
